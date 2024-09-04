f9adc3b0ee499ebbf7b9e1247922ea9449a68060
KonradStaniec <konrad.staniec@gmail.com>
2024-09-04 16:22:41 +0200

    remove zoneconcierge

--- app/upgrades/signetlaunch/upgrades.go
+++ app/upgrades/signetlaunch/upgrades.go
@@ -23,10 +23,17 @@ import (
 	btclighttypes "github.com/babylonlabs-io/babylon/x/btclightclient/types"
 )
 
+const (
+	ZoneConciergeStoreKey = "zoneconcierge"
+)
+
 var Upgrade = upgrades.Upgrade{
 	UpgradeName:          "signet-launch",
 	CreateUpgradeHandler: CreateUpgradeHandler,
-	StoreUpgrades:        store.StoreUpgrades{},
+	// Upgrade necessary for deletions of `zoneconcierge`
+	StoreUpgrades: store.StoreUpgrades{
+		Deleted: []string{ZoneConciergeStoreKey},
+	},
 }
 
 // CreateUpgradeHandler upgrade handler for launch.
